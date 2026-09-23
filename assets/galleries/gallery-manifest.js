2136647d1116ee8eeec4163c393c4a45c4b26e2c
NEEMAN01 <97268721+NEEMAN01@users.noreply.github.com>
2026-09-23 11:43:33 +0300

    Add initial automatic gallery manifest

---
+++ assets/galleries/gallery-manifest.js
@@ -0,0 +1,47 @@
+window.GALLERY_MANIFEST = {
+  "version": 1,
+  "generatedAt": "2026-09-23T00:00:00Z",
+  "categories": {
+    "canteen": [
+      {"path":"assets/galleries/canteen/canteen-community-care-september-2026.jpg","name":"canteen-community-care-september-2026.jpg","kind":"photo","added":""},
+      {"path":"assets/galleries/canteen/canteen-september-2026.jpg","name":"canteen-september-2026.jpg","kind":"photo","added":""},
+      {"path":"assets/galleries/canteen/canteen-main.png","name":"canteen-main.png","kind":"legacy","added":""},
+      {"path":"assets/galleries/canteen/canteen-overview.png","name":"canteen-overview.png","kind":"legacy","added":""},
+      {"path":"assets/galleries/canteen/canteen-gallery.jpg","name":"canteen-gallery.jpg","kind":"legacy","added":""}
+    ],
+    "college": [
+      {"path":"assets/galleries/college/christian-leadership-university-2024.jpg","name":"christian-leadership-university-2024.jpg","kind":"photo","added":""},
+      {"path":"assets/galleries/college/college-overview.png","name":"college-overview.png","kind":"legacy","added":""},
+      {"path":"assets/galleries/college/college-gallery.jpg","name":"college-gallery.jpg","kind":"legacy","added":""}
+    ],
+    "friends": [
+      {"path":"assets/galleries/friends/friends-community-collage-2026.jpg","name":"friends-community-collage-2026.jpg","kind":"photo","added":""},
+      {"path":"assets/galleries/friends/friends-faith-community-2026.jpg","name":"friends-faith-community-2026.jpg","kind":"photo","added":""},
+      {"path":"assets/galleries/friends/friends-faith-friendship-support-2026.jpg","name":"friends-faith-friendship-support-2026.jpg","kind":"photo","added":""},
+      {"path":"assets/galleries/friends/friends-supporters-couple-2026.jpg","name":"friends-supporters-couple-2026.jpg","kind":"photo","added":""},
+      {"path":"assets/galleries/friends/friends-supporters-family-2026.jpg","name":"friends-supporters-family-2026.jpg","kind":"photo","added":""},
+      {"path":"assets/galleries/friends/friends-supporters-group-2026.jpg","name":"friends-supporters-group-2026.jpg","kind":"photo","added":""},
+      {"path":"assets/galleries/friends/friends-overview.png","name":"friends-overview.png","kind":"legacy","added":""},
+      {"path":"assets/galleries/friends/friends-gallery.jpg","name":"friends-gallery.jpg","kind":"legacy","added":""}
+    ],
+    "holidays": [
+      {"path":"assets/galleries/holidays/rosh-hashanah-01.jpg","name":"rosh-hashanah-01.jpg","kind":"photo","added":""},
+      {"path":"assets/galleries/holidays/rosh-hashanah-02.jpg","name":"rosh-hashanah-02.jpg","kind":"photo","added":""},
+      {"path":"assets/galleries/holidays/rosh-hashanah-03.jpg","name":"rosh-hashanah-03.jpg","kind":"photo","added":""},
+      {"path":"assets/galleries/holidays/rosh-hashanah-04.jpg","name":"rosh-hashanah-04.jpg","kind":"photo","added":""},
+      {"path":"assets/galleries/holidays/rosh-hashanah-05.jpg","name":"rosh-hashanah-05.jpg","kind":"photo","added":""},
+      {"path":"assets/galleries/holidays/holidays-overview.png","name":"holidays-overview.png","kind":"legacy","added":""}
+    ],
+    "holocaust": [
+      {"path":"assets/galleries/holocaust/holocaust-overview.png","name":"holocaust-overview.png","kind":"legacy","added":""},
+      {"path":"assets/galleries/holocaust/holocaust-gallery.jpg","name":"holocaust-gallery.jpg","kind":"legacy","added":""}
+    ],
+    "repatriants": [
+      {"path":"assets/galleries/repatriants/repatriants-overview.png","name":"repatriants-overview.png","kind":"legacy","added":""},
+      {"path":"assets/galleries/repatriants/repatriants-overview-1.png","name":"repatriants-overview-1.png","kind":"legacy","added":""},
+      {"path":"assets/galleries/repatriants/repatriants-overview-2.png","name":"repatriants-overview-2.png","kind":"legacy","added":""},
+      {"path":"assets/galleries/repatriants/repatriants-gallery-1.jpg","name":"repatriants-gallery-1.jpg","kind":"legacy","added":""},
+      {"path":"assets/galleries/repatriants/repatriants-gallery-2.jpg","name":"repatriants-gallery-2.jpg","kind":"legacy","added":""}
+    ]
+  }
+};
